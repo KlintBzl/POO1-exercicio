22d478a24611fe7935b966e516c2ed4bba981daa
KlintBzl <klintscouts@gmail.com>
2025-02-28 16:22:47 -0300

    Exercicio5 realizado

---
+++ Atividade5/src/atividade5/Atividade5.java
@@ -0,0 +1,10 @@
+package atividade5;
+
+public class Atividade5 {
+    
+    public static void main(String[] args) {
+        Funcionario pessoa1 = new Funcionario("Edivaldo", 1500);
+        pessoa1.exibir();
+    }
+    
+}
